3bcf0f70402ae378b030747f1f37acfcdaeb8fea
System Administrator <root@shivam.local>
2018-07-20 18:19:23 -0400

    added parser class

--- src/main/java/com/wyds/packets/PacketParser.java
+++ src/main/java/com/wyds/packets/PacketParser.java
@@ -1,4 +1,48 @@
 package com.wyds.packets;
 
+import io.pkts.PacketHandler;
+import io.pkts.Pcap;
+import io.pkts.packet.Packet;
+import io.pkts.protocol.Protocol;
+
+import java.io.IOException;
+
 public class PacketParser {
+
+    public static void main(String[] args) throws IOException {
+
+        final Pcap pcap = Pcap.openStream("output.pcap");
+
+        pcap.loop(new PacketHandler() {
+            @Override
+            public boolean nextPacket(Packet packet) throws IOException {
+                if(packet.hasProtocol(Protocol.ARP)){
+
+                }
+                if(packet.hasProtocol(Protocol.TCP)){
+
+                }
+                if(packet.hasProtocol(Protocol.UDP)){
+
+                }
+                if(packet.hasProtocol(Protocol.TLS)){
+
+                }
+                if(packet.hasProtocol(Protocol.IPv4)){
+
+                }
+                if(packet.hasProtocol(Protocol.)){
+
+                }
+
+
+
+                return false;
+            }
+        });
+
+    }
+
+
+
 }
