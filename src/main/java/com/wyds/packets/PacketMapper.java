c5fb44735b53e8a370cabc3ff3b6872387cec358
Shivam Patel <shivamp@shivam.local>
2018-08-07 10:58:19 -0400

    cleaned package and running with dummy data

--- src/main/java/com/wyds/packets/PacketMapper.java
+++ src/main/java/com/wyds/packets/PacketMapper.java
@@ -1,18 +1,15 @@
 package com.wyds.packets;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PacketMapper {
 
     private PacketParser parser = new PacketParser();
-    private Map<Integer, String> packetMap = new HashMap<>();
-    private List<String> packetLst = new LinkedList<>();
+    //private Map<Integer, String> packetMap = new HashMap<>();
+    private static List<String> packetLst = new ArrayList<>();
 
-    public Map<Integer, String> packet() throws IOException {
+    /*public Map<Integer, String> packet() throws IOException {
         parser.parseUDP();
 
         for (int i = 0; i < parser.getPacketList().size(); i++) {
@@ -22,13 +19,15 @@ public class PacketMapper {
         }
         //System.out.println(packetMap);
         return packetMap;
-    }
-
-
-   /* public List<String> packet1() throws IOException {
-        parser.parseUDP();
-        packetLst.addAll(parser.getPacketList());
-        return packetLst;
     }*/
+
+
+    public List<String> packetL() throws IOException {
+        parser.parseUDP();
+        packetLst.add(0, "not null");
+        packetLst.addAll(parser.getPacketList());
+        System.out.println(packetLst);
+        return packetLst;
+    }
 
 }
